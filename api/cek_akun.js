c30b6b561c78271235b1be27b3f02134defb5bcf
Masd-cd <fitriediarsihpf@gmail.com>
2026-03-24 12:21:27 +0700

    Update cek_akun.js

--- api/cek_akun.js
+++ api/cek_akun.js
@@ -1,23 +1,26 @@
 export default async function handler(req, res) {
     const orderId = req.query.order_id;
-    if (!orderId) return res.status(400).json({ error: 'Order ID kosong' });
-
     const kvUrl = process.env.KV_REST_API_URL;
     const kvToken = process.env.KV_REST_API_TOKEN;
 
+    if (!kvUrl || !kvToken) return res.status(500).json({ error: 'Database Config Missing' });
+
     try {
-        // Mengambil data dari Vercel KV berdasarkan Order ID
         const response = await fetch(`${kvUrl}/get/${orderId}`, {
             headers: { 'Authorization': `Bearer ${kvToken}` }
         });
         const data = await response.json();
 
         if (data.result) {
-            return res.status(200).json({ status: 'sukses', akun: JSON.parse(data.result) });
+            // Karena kita simpan dalam bentuk { result: "JSON_STRING" }
+            const parseLapis1 = JSON.parse(data.result);
+            const dataAkunAsli = JSON.parse(parseLapis1.result);
+            
+            return res.status(200).json({ status: 'sukses', akun: dataAkunAsli });
         } else {
             return res.status(200).json({ status: 'menunggu' });
         }
     } catch (error) {
-        return res.status(500).json({ error: 'Gagal mengambil data' });
+        return res.status(500).json({ error: 'Gagal ambil data' });
     }
 }
